30adde6d52e4c978e9945373fe58befcbe8dc88a
Zoe Thompson <136368115+zoethompson213@users.noreply.github.com>
2023-06-20 14:17:50 -0600

    Updating Zoe's version of toy model files

--- Zoe_version/main.R
+++ Zoe_version/main.R
@@ -2,11 +2,12 @@
 # main
 
 # Source the files we use (data, diff equation, model structure)
-source('data.R')
-source('diffeq.R')
-source('model_structure.R')
+source('Zoe_version/data.R')
+#source('Zoe_version/diffeq.R')
+source('Zoe_version/model_structure.R')
 
 # create a starting state
+start.state.2011 = set.state(c(MD.UNINFECTED.2011, MD.UNDIAGNOSED.2011, MD.DIAGNOSES.2011)) #this order matters#
 
 # set up parameters
 
